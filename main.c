7d49740cce8086b121ecb52224238acbb53c3b7f
Connor McLeod <cn248485@dal.ca>
2023-06-21 17:20:29 -0300

    Added generic entity struct and began implementation of coin

--- main.c
+++ main.c
@@ -1,21 +1,9 @@
-// Normal definitions.
-#include <stdio.h>
-#include <gb/gb.h>
-#include <gbdk/font.h>
+#include "header.h"
 
 // Tilesets created by gbtd.
 #include "player.c"
 #include "demon.c"
 #include "window.c"
-
-// Bit mask to determine only the joypad bit that relates to D-pad button input.
-#define d_pad_bit(x) (x & 0x0F)
-
-// Playable space restrictions.
-#define X_MIN 0x08
-#define X_MAX 0xA0
-#define Y_MIN 0x10
-#define Y_MAX 0x90
 
 int16_t player_coords[2];    // Format: x, y
 uint8_t player_has_collided_x_min = 0;
@@ -264,9 +252,22 @@ void player_movement()
 // Displays the title screen. When user presses the START button the code continues to execute.
 void title_screen()
 {
-    printf("DEMON CHASE\n\nWEAKBOX INSTUSTRIES 2023\n\n\n\n\nPRESS START");
+    printf("DEMON CHASE\n\nWEAKBOX INSTUSTRIES 2023\n\n\n\n\nPRESS START\n\n\n\n");
     waitpad(J_START);
     printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+}
+
+// Displays the death screen.
+void death_screen()
+{
+    play_bonk_sfx();
+    printf("YOU DIED\n\n\n");
+}
+
+// Checks for collisions between the player and the demon.
+UBYTE check_collisions(Demon* demon)
+{
+    return(((player_coords[0] >= demon->pos_x && player_coords[0] <= demon->pos_x + 0x08) && (player_coords[1] >= demon->pos_y && player_coords[1] <= demon->pos_y + 0x08)) || ((demon->pos_x >= player_coords[0] && demon->pos_x <= player_coords[0] + 0x08) && (demon->pos_y >= player_coords[1] && demon->pos_y <= player_coords[1] + 0x08)));
 }
 
 void main()
@@ -274,36 +275,59 @@ void main()
     load_font(font_min);
     init_sound();
 
-    title_screen();
-
-    // Setup player sprite.
-    set_sprite_data(0, 9, player);
-    set_sprite_tile(0, 0);
-    player_coords[0] = 0x18;
-    player_coords[1] = 0x20;
-    move_sprite(0, player_coords[0], player_coords[1]);
-    
-    // Setup demon sprite.
-    set_sprite_data(9, 9, demon);
-    set_sprite_tile(1, 9);
-
-    Demon demon;
-    demon.pos_x = 0x80; // Sets the demon's spawnpoint coordinates.
-    demon.pos_y = 0x80;
-    demon.timer = 0;
-    move_sprite(1, demon.pos_x, demon.pos_y);   // Spawns the demon in at his spawn coordinates.
-
-    SHOW_SPRITES;
-
-    set_win_tiles(0, 0, 19, 1, window);
-    move_win(0x08, 0x88);
-    SHOW_WIN;
-    DISPLAY_ON;
-
+    UBYTE state = 0;
+    Demon _demon;
     while(1)
     {
-        player_movement();
-        demon_logic(&demon);
-        wait_vbl_done();    // Seems to frame limit to 60 fps.
+        switch(state)
+        {
+            case 0: // Title screen state.
+                title_screen();
+
+                // Setup player sprite.
+                set_sprite_data(0, 9, player);
+                set_sprite_tile(0, 0);
+                player_coords[0] = 0x18;
+                player_coords[1] = 0x20;
+                move_sprite(0, player_coords[0], player_coords[1]);
+                
+                // Setup demon sprite.
+                set_sprite_data(9, 9, demon);
+                set_sprite_tile(1, 9);
+
+                state++;
+                break;
+
+            case 1: // Game state.
+                _demon.pos_x = 0x80; // Sets the demon's spawnpoint coordinates.
+                _demon.pos_y = 0x80;
+                _demon.timer = 0;
+                move_sprite(1, _demon.pos_x, _demon.pos_y);   // Spawns the demon in at his spawn coordinates.
+
+                SHOW_SPRITES;
+
+                set_win_tiles(0, 0, 19, 1, window);
+                move_win(0x08, 0x88);
+                SHOW_WIN;
+                DISPLAY_ON;
+
+                init_coin(&coin, 100, 100);
+
+                while(!check_collisions(&_demon))
+                {
+                    player_movement();
+                    demon_logic(&_demon);
+                    wait_vbl_done();    // Seems to frame limit to 60 fps.
+                }
+
+                state++;
+                break;
+
+            case 2: // Game over state.
+                death_screen();
+                delay(1000);
+                state = 0;
+                break;
+        }
     }
 }
--- make.bat
+++ make.bat
@@ -1,1 +1,1 @@
-c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -Wm-ys -o first_project.gb main.c
+c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -Wm-ys -o first_project.gb main.c coin.c
